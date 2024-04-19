f1b58982bae953290036cbc5c90a5b71b1d493f4
Pradeep Mohan <mspr.utilities@gmail.com>
2024-04-19 20:55:18 +0000

    secrets manager added

---
+++ notebooks/config.py
@@ -0,0 +1,40 @@
+# Import the Secret Manager client library.
+from google.cloud import secretmanager
+
+# GCP project in which to store secrets in Secret Manager.
+project_id = "vertexai-gemini-hackathon-2024"
+
+# ID of the secret to create.
+secret_id = "projects/931568092155/secrets/service_account_key
+
+"
+
+# Create the Secret Manager client.
+client = secretmanager.SecretManagerServiceClient()
+
+# Build the parent name from the project.
+parent = f"projects/{project_id}"
+
+# Create the parent secret.
+secret = client.create_secret(
+    request={
+        "parent": parent,
+        "secret_id": secret_id,
+        "secret": {"replication": {"automatic": {}}},
+    }
+)
+
+# Add the secret version.
+version = client.add_secret_version(
+    request={"parent": secret.name, "payload": {"data": b"hello world!"}}
+)
+
+# Access the secret version.
+response = client.access_secret_version(request={"name": version.name})
+
+# Print the secret payload.
+#
+# WARNING: Do not print the secret in a production environment - this
+# snippet is showing how to access the secret material.
+payload = response.payload.data.decode("UTF-8")
+print(f"Plaintext: {payload}")
