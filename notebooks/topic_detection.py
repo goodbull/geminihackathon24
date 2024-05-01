66857f45191d44a83db6d5ce1c3ed10e4022a9fe
pradeepmohansml <107280156+pradeepmohansml@users.noreply.github.com>
2024-04-30 23:26:45 -0400

    topic extraction init

---
+++ notebooks/topic_detection.py
@@ -0,0 +1,3 @@
+#Structured Topic Detection Tool
+#Makes use of Vertex AI, Gemini 1.5 Pro to Detect Common topics in a video's frames and transcript using BERTopic 
+#Returns: List[str]: a collection of topics
